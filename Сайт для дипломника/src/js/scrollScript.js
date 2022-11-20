e6ec180cd763d040718bc9728a67c1382f680f0a
Кирилл <iorbit4557@gmail.com>
2022-11-20 12:49:59 +0300

    Добавлены переходы при нажатии и переименован скрипт

---
+++ Сайт для дипломника/src/js/scrollScript.js
@@ -0,0 +1,147 @@
+'use strict';
+
+$(document).ready(function(){
+    $("#menu_1").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -240;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#leadb").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -240;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#ser").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+
+
+$(document).ready(function(){
+    $("#art_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#leg_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#kn_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#ost_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#blo").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#rec_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#artic_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#qual_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
+
+$(document).ready(function(){
+    $("#pat_fr").on("click", "a", function (event){
+        event.preventDefault();
+        
+        var id = $(this).attr('href'),
+            top = $(id).offset().top -130;
+        $('body,html').animate({scrollTop: top}, 1500);
+        
+    });
+    
+});
