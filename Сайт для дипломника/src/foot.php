00c07b157d0fe1611d4c31518ce906f0ed37a639
Кирилл <iorbit4557@gmail.com>
2022-11-30 19:26:15 +0300

    Адаптив и страницы
    1. Почти разработан адаптив страницы index.html
    2. Добавлены новые страницы: about, science, education, services

---
+++ Сайт для дипломника/src/foot.php
@@ -0,0 +1,9 @@
+<footer>
+            <div class="footmenu">
+                <ul>
+                    <li><a href="#">doc.Cherevatyy</a></li>
+                    <li><a href="#">контакты</a></li>
+                    <li><a href="#">помощь/FAQ</a></li>
+                </ul>
+            </div>
+        </footer>
