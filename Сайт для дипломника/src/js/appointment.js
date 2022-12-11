4ee5b2e86f76d8133a9f408d92c7d7e1db825ae5
Кирилл <iorbit4557@gmail.com>
2022-12-11 15:15:35 +0300

    Разработано появляющаяся форма

---
+++ Сайт для дипломника/src/js/appointment.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$('#app').click(function (event) {
+		$('.appointment_frame2.disapp,.appointment2, body').toggleClass('appear');
+		$('body').toggleClass('lock');
+	});
+	$('#but').click(function (event) {
+		$('.appointment_frame2.disapp,.appointment2, body').toggleClass('');
+	});
+});
