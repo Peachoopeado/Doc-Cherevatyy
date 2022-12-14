a2062f81d8576e10a51415621233d09f1a368101
Кирилл <iorbit4557@gmail.com>
2022-12-14 16:31:42 +0300

    Ссылочная структура+сплывающая форма

--- Сайт для дипломника/src/js/appointment.js
+++ Сайт для дипломника/src/js/appointment.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-	$('#app').click(function (event) {
-		$('.appointment_frame2.disapp,.appointment2, body').toggleClass('appear');
+	$('#app,.appointment_frame2.disapp,.service_button').click(function (event) {
+		$('.appointment_frame2.disapp,.appointment2.disapp, body').toggleClass('appear');
 		$('body').toggleClass('lock');
 	});
-	$('#but').click(function (event) {
+	$('.service_frame').click(function (event) {
 		$('.appointment_frame2.disapp,.appointment2, body').toggleClass('');
 	});
 
